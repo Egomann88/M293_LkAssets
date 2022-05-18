5953323e118a54a9726cda8454f360856226170d
Justin <justin@kauz.ch>
2022-05-18 11:41:12 +0200

    Teil 1

---
+++ src/script.js
@@ -0,0 +1,4 @@
+window.onload = () => {
+  //alert('Aboniert meinen Kanal: Gilganos Playing Games');
+  console.log('%cAboniert meinen Kanal: %cGilganos Playing Games', 'font-size: 30px;', 'font-size: 30px; color: blue; font-weight: bold');
+}
